70a466f52d61b20e5997a1a9307f6102a496b952
Sivaani <sivaanijanaswamy@gmail.com>
2025-05-21 19:25:04 +0530

    case matching

--- src/components/GSpace/GSpaceCard.jsx
+++ src/components/GSpace/GSpaceCard.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import GSpaceTag from './GspaceTag';
+import GSpaceTag from './GSpaceTag';
 
 const GSpaceCard = ({ title, description, tags, slug }) => {
   const navigate = useNavigate();
